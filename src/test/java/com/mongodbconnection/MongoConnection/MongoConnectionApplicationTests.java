99373287340b0a744ff29a2953961783427f69b1
SushilMallMobikwiks <sushil.mall@mobikwik.com>
2020-02-22 16:22:36 +0530

    first commit

---
+++ src/test/java/com/mongodbconnection/MongoConnection/MongoConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongodbconnection.MongoConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
